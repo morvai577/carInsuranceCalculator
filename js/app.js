542ae5239d8ae30503b6ec2553633bf9d5270f39
Vaibhav <vaibhavmor@Vaibhavs-iMac.local>
2018-07-27 22:31:11 +1200

    Add function to calculate insurance

--- js/app.js
+++ js/app.js
@@ -24,13 +24,91 @@ function eventListeners() {
         // Check that all the fields have something
         if (make === '' || year === '' || level === '') {
             html.displayError('All the fields are mandatory!');
-        } else {console.log('Alright!');}
+        } else {
+            // Make the quotation
+            const insurance = new Insurance(make, year, level);
+            const price = insurance.calculateQuotation(insurance);
+
+            // Print the result from HTMLUI();
+            html.showResults(price);
+        }
     });
 }
 
 
 // Objects
 
+// Everything related to the quotation and calculations is Insurance
+function Insurance(make, year, level) {
+    this.make = make;
+    this.year = year;
+    this.level = level;
+}
+
+// Calculate the price for the current quotation
+Insurance.prototype.calculateQuotation = function(insurance) {
+    let price;
+    const base = 2000;
+
+    // get the make
+    const make = insurance.make;
+
+    /*
+        1 = American 1.15%
+        2 = Asian 1.05%
+        3 = European 35%
+    */
+
+    switch(make) {
+        case '1':
+            price = base * 1.15;
+            break;
+        case '2':
+            price = base * 1.05;
+            break;
+        case '3':
+            price = base * 1.35;
+            break;
+    }
+
+    // Get the year
+    const year = insurance.year;
+
+    // Get the years difference
+    const difference = this.getYearDifference(year);
+
+    // Each year the cost of the insurance is going to be 3% cheaper
+    //price = price - ((difference * 3) * price) / 100; (NEED TO DEBUG!)
+
+    // Check the level of protection;
+    const level = insurance.level
+
+    price = this.calculateLevel(price, level);
+
+    return price;
+}
+
+// Returns the difference between years
+Insurance.prototype.getYearDifference = function(year) {
+    return new Date().getFullYear - year;
+}
+
+// Adds the value based on the level of protection
+Insurance.prototype.calculateLevel = function(price, level) {
+    /*
+        Basic insurance is going to increase the value by 30%
+        Complete Insurance is going to increasethe value by 50%
+    */
+   if(level === 'basic') {
+       price = price * 1.30;
+   } else {
+       price = price * 1.50
+   }
+
+   return price;
+}
+
+// Everything related to the HTML
 function HTMLUI() {}
 
 // Displays the latest 20 years in the select
@@ -69,3 +147,4 @@ HTMLUI.prototype.displayError = function(message) {
         document.querySelector('.error').remove();
     }, 3000);
 }
+
